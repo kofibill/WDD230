bdbb155310210dbc33c66b533e340f79d0ea27c1
kofibill <jessekim064@gmail.com>
2024-09-17 02:11:48 +0000

    deleted

--- trials.py
+++
@@ -1,8 +0,0 @@
-first_name = input('Please enter your first name: '.capitalize())
-last_name = input('Please enter your last name: '.capitalize())
-age = int(input('Please enter your age here: '))
-school = input('What is the name of your school: '.title())
-print()
-
-print(f'My full-name is {first_name} {last_name}. I am {age} years of age.')
-print(f'I attend {school}')
